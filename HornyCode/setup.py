61939bdaba989059b9a8871eee5b247159804541
Mateusz <idk2137xda@gmail.com>
2025-04-18 23:26:35 +0200

    Update setup.py

--- HornyCode/setup.py
+++ HornyCode/setup.py
@@ -4,7 +4,7 @@ setup(
     name='HornyCode',
     version='0.1.1',
     packages=find_packages(),
-    description='OwO, what's this',
+    description='OwO, whats this',
     author='MateuszDubaj',
     python_requires='>=3.6',
     entry_points={
